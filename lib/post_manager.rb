9cff459a46aaa785bb0af399b94dfd7fcac8b2c2
karydja <karydjasouza@outlook.com>
2015-07-15 19:40:23 -0300

    Fixes #2

--- lib/post_manager.rb
+++ lib/post_manager.rb
@@ -1,8 +1,9 @@
 class PostManager
-  def self.get_posts(fan_page_id, limit = 25)
+  def self.get_posts(fan_page_id, limit = 25, fields = [])
     begin
+      fields = ["from{name}", "message"] if fields.blank?
       facebook = Koala::Facebook::API.new(ENV["FACEBOOK_ACCESS_TOKEN"])
-      response = facebook.get_object("#{fan_page_id}/feed?fields=from{name},message&limit=#{limit}")
+      response = facebook.get_object("#{fan_page_id}/feed?fields=#{fields.join(",")}&limit=#{limit}")
       response_objects = convert_to_object(response)
     rescue Koala::Facebook::APIError => e
       nil
